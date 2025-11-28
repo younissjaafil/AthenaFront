d5f5e112de6b32543caa680764f59dd1120a3217
Younissjaafil <youniss@3vo.me>
2025-11-28 23:43:16 +0200

    fix : deployment issue cause by env

--- .next/static/chunks/app_student_layout_tsx_b3d7ee68._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_5eca6c0a._.js",
-  "static/chunks/_261d7816._.js"
-],
-    source: "dynamic"
-});
